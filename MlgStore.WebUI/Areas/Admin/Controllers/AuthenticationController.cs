0c39c5cbc4d91336589f71cd7c6fb0015f651e3c
mehmetmalgir <mmalgr@gmail.com>
2023-02-18 22:46:03 +0300

    Add project files.

---
+++ MlgStore.WebUI/Areas/Admin/Controllers/AuthenticationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MlgStore.WebUI.Areas.Admin.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
