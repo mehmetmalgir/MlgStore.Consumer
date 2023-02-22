0160e713b1e3cbd53078203b357857d1ea49da57
mehmetmalgir <mmalgr@gmail.com>
2023-02-22 18:50:57 +0300

    .

---
+++ MlgStore.WebUI/Areas/Admin/Models/ApiProductPhotos.cs
@@ -0,0 +1,13 @@
+﻿namespace MlgStore.WebUI.Areas.Admin.Models
+{
+    public class ApiProductPhotos
+    {
+
+        public string Picture { get; set; }
+        public int ProductID { get; set; }
+        public ApiProductDto Product { get; set; }
+
+
+
+    }
+}
