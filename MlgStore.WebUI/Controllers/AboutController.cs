0a6ea76469e4a1355b97d6689b5ee55a02f385f8
mehmetmalgir <mmalgr@gmail.com>
2023-03-04 18:53:20 +0300

    "About" Page Created

---
+++ MlgStore.WebUI/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MlgStore.WebUI.Controllers
+{
+	public class AboutController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
