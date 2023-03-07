94caccc56f9383ea8372c641d2787e1b0fb3d8f0
mehmetmalgir <mmalgr@gmail.com>
2023-03-07 23:15:16 +0300

    Customer Register Created

--- MlgStore.WebUI/Controllers/CustomerAuthenticationController.cs
+++ MlgStore.WebUI/Controllers/CustomerAuthenticationController.cs
@@ -5,7 +5,11 @@ using MlgStore.WebUI.Models.Contexts;
 using MlgStore.WebUI.Models.Dtos;
 using MlgStore.WebUI.Models.Validations;
 using Newtonsoft.Json;
+using System;
 using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Text;
 
 namespace MlgStore.WebUI.Controllers
 {
@@ -55,7 +59,64 @@ namespace MlgStore.WebUI.Controllers
 
         }
 
+        [HttpPost]
+        public IActionResult Register(NewCustomerUserDto dto)
+        {
+            NewCustomerUserDtoValidator validator = new NewCustomerUserDtoValidator();
+            var result = validator.Validate(dto);
 
+            if(result.IsValid)
+            {
+                HttpClient client = new HttpClient();
+
+                client.BaseAddress = new Uri("https://localhost:44365/api/Customers");
+
+
+                var jsonStr = JsonConvert.SerializeObject(new
+                {
+                    FirstName = dto.FirstName,
+                    LastName = dto.LastName,
+                    Address1 = dto.Address1,
+                    Address2 = dto.Address2,
+                    City = dto.City,
+                    District = dto.District,
+                    Phone = dto.Phone,
+                    Email = dto.Email,
+                    Password = dto.Password,
+                    RegistirationDate = DateTime.Now
+                });
+
+
+
+                StringContent content = new StringContent(jsonStr, Encoding.UTF8, "application/json");
+
+
+                HttpResponseMessage msg = client.PostAsync(client.BaseAddress, content).Result;
+
+                if (msg.StatusCode == HttpStatusCode.Created)
+                {
+                    return Json(new { IsSuccess = true, Message = "Yeni Üye Oluşturuldu" });
+                }
+
+                return Json(new { IsSuccess = false });
+
+
+            }
+            else
+            {
+                string errorMessages = "<div class = 'alert alert-warning'>";
+                foreach (ValidationFailure item in result.Errors)
+                {
+                    errorMessages += $"<div>{item.ErrorMessage}</div>";
+                }
+
+                errorMessages += "</div>";
+
+                return Json(new { isSuccess = false, Message = errorMessages });
+            }
+
+
+        }
 
 
 
---
+++ MlgStore.WebUI/Models/Validations/NewCustomerUserDtoValidator.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+using MlgStore.WebUI.Models.Dtos;
+
+namespace MlgStore.WebUI.Models.Validations
+{
+    public class NewCustomerUserDtoValidator : AbstractValidator<NewCustomerUserDto>
+    {
+        public NewCustomerUserDtoValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email Boş Bırakılamaz");
+
+            RuleFor(x => x.Address1)
+                .NotEmpty()
+                .WithMessage("Adres Boş Bırakılamaz");            
+
+            RuleFor(x => x.FirstName)
+                .NotEmpty()
+                .WithMessage("İsim Boş Bırakılamaz");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty()
+                .WithMessage("Soyisim Boş Bırakılamaz");
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage("Şifre Boş Bırakılamaz");
+
+            RuleFor(x => x.Phone)
+                .NotEmpty()
+                .WithMessage("Telefon Boş Bırakılamaz");
+
+            RuleFor(x => x.City)
+                .NotEmpty()
+                .WithMessage("İl Boş Bırakılamaz");
+
+            RuleFor(x => x.District)
+                .NotEmpty()
+                .WithMessage("İlçe Boş Bırakılamaz");
+
+
+
+        }
+
+
+    }
+}
